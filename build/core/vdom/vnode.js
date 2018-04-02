55722410193957eae0e4dd06cbc57ee47f9dfcbd
zhangge <zhangge@zhongan.com>
2018-04-02 17:05:30 +0800

    flow to es5

---
+++ build/core/vdom/vnode.js
@@ -0,0 +1,78 @@
+export default class VNode {
+  // functional scope id support
+
+  // real context vm for functional nodes
+  // async component factory function
+  // is a cloned node?
+  // necessary for enter transition check
+  // contains raw HTML? (server only)
+  // component instance
+  constructor(tag, data, children, text, elm, context, componentOptions, asyncFactory) {
+    this.tag = tag;
+    this.data = data;
+    this.children = children;
+    this.text = text;
+    this.elm = elm;
+    this.ns = undefined;
+    this.context = context;
+    this.fnContext = undefined;
+    this.fnOptions = undefined;
+    this.fnScopeId = undefined;
+    this.key = data && data.key;
+    this.componentOptions = componentOptions;
+    this.componentInstance = undefined;
+    this.parent = undefined;
+    this.raw = false;
+    this.isStatic = false;
+    this.isRootInsert = true;
+    this.isComment = false;
+    this.isCloned = false;
+    this.isOnce = false;
+    this.asyncFactory = asyncFactory;
+    this.asyncMeta = undefined;
+    this.isAsyncPlaceholder = false;
+  }
+
+  // DEPRECATED: alias for componentInstance for backwards compat.
+  /* istanbul ignore next */
+  // for SSR caching
+  // is a v-once node?
+  // empty comment placeholder?
+  // hoisted static node
+  // component placeholder node
+
+  // strictly internal
+  // rendered in this component's scope
+  get child() {
+    return this.componentInstance;
+  }
+}
+
+export const createEmptyVNode = (text = '') => {
+  const node = new VNode();
+  node.text = text;
+  node.isComment = true;
+  return node;
+};
+
+export function createTextVNode(val) {
+  return new VNode(undefined, undefined, undefined, String(val));
+}
+
+// optimized shallow clone
+// used for static nodes and slot nodes because they may be reused across
+// multiple renders, cloning them avoids errors when DOM manipulations rely
+// on their elm reference.
+export function cloneVNode(vnode) {
+  const cloned = new VNode(vnode.tag, vnode.data, vnode.children, vnode.text, vnode.elm, vnode.context, vnode.componentOptions, vnode.asyncFactory);
+  cloned.ns = vnode.ns;
+  cloned.isStatic = vnode.isStatic;
+  cloned.key = vnode.key;
+  cloned.isComment = vnode.isComment;
+  cloned.fnContext = vnode.fnContext;
+  cloned.fnOptions = vnode.fnOptions;
+  cloned.fnScopeId = vnode.fnScopeId;
+  cloned.asyncMeta = vnode.asyncMeta;
+  cloned.isCloned = true;
+  return cloned;
+}
---
+++ build/platforms/weex/compiler/modules/recycle-list/component-root.js
@@ -0,0 +1,11 @@
+import { addAttr } from 'compiler/helpers';
+
+// mark component root nodes as
+export function postTransformComponentRoot(el, options) {
+  if (!el.parent) {
+    // component root
+    addAttr(el, '@isComponentRoot', 'true');
+    addAttr(el, '@templateId', '_uid');
+    addAttr(el, '@componentProps', '$props || {}');
+  }
+}
